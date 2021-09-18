b639f13a273f0c25017af4a68d1885ea0ccb356a
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2021-09-18 13:49:27 -0300

    Initial Commit

---
+++ swiftUI-015-Landmarks/swiftUI_015_LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_015_LandmarksApp.swift
+//  swiftUI-015-Landmarks
+//
+//  Created by Luiz Carlos da Silva Araujo on 18/09/21.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_015_LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
