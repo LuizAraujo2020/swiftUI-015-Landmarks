a4ccea5a9ec4e8280d2df183784e620af87f6578
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2021-09-19 10:58:07 -0300

    :gift: Adopt the Model Object in Your Views

--- swiftUI-015-Landmarks/swiftUI_015_LandmarksApp.swift
+++ swiftUI-015-Landmarks/swiftUI_015_LandmarksApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct swiftUI_015_LandmarksApp: App {
+    @StateObject private var modelData = ModelData()
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(modelData)
         }
     }
 }
