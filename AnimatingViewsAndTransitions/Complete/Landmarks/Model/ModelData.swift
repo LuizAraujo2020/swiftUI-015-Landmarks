1e7b45555671661acacbd8d19a34f6af7789fe94
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2021-09-19 14:50:50 -0300

    :gift: Combine the Badge Foreground and Background

--- AnimatingViewsAndTransitions/Complete/Landmarks/Model/ModelData.swift
+++ AnimatingViewsAndTransitions/Complete/Landmarks/Model/ModelData.swift
@@ -1,9 +1,9 @@
-//
-//  ModelData.swift
-//  swiftUI-015-Landmarks
-//
-//  Created by Luiz Carlos da Silva Araujo on 18/09/21.
-//
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+Storage for model data.
+*/
 
 import Foundation
 import Combine
@@ -11,25 +11,22 @@ import Combine
 final class ModelData: ObservableObject {
     @Published var landmarks: [Landmark] = load("landmarkData.json")
     var hikes: [Hike] = load("hikeData.json")
-
 }
-
-
 
 func load<T: Decodable>(_ filename: String) -> T {
     let data: Data
-    
+
     guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
-    else {
-        fatalError("Couldn't find \(filename) in main bundle.")
+        else {
+            fatalError("Couldn't find \(filename) in main bundle.")
     }
-    
+
     do {
         data = try Data(contentsOf: file)
     } catch {
         fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
     }
-    
+
     do {
         let decoder = JSONDecoder()
         return try decoder.decode(T.self, from: data)
@@ -37,3 +34,4 @@ func load<T: Decodable>(_ filename: String) -> T {
         fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
     }
 }
+
