c6a602fe17638d33c2e22e051f5d4ce8117ba651
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2021-09-18 21:38:59 -0300

    :gift: Create the Row View

---
+++ LandmarkRow.swift
@@ -0,0 +1,29 @@
+//
+//  LandmarkRow.swift
+//  swiftUI-015-Landmarks
+//
+//  Created by Luiz Carlos da Silva Araujo on 18/09/21.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    let landmark: Landmark
+    
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+            
+            Spacer()
+        }
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkRow(landmark: landmarks[0])
+    }
+}
