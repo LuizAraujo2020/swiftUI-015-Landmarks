b27ec8dd6796f41fe9060849f7e1bacf6abf8140
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2021-09-18 21:01:24 -0300

    :gift: Building Lists and Navigation

---
+++ README.md
@@ -0,0 +1,8 @@
+#  <#Title#>
+
+https://developer.apple.com/tutorials/swiftui/creating-and-combining-views
+
+
+Create a view for a Image and Crop it
+Use MapKit
+
---
+++ swiftUI-015-Landmarks/CircleImage.swift
@@ -0,0 +1,23 @@
+//
+//  CircleImage.swift
+//  swiftUI-015-Landmarks
+//
+//  Created by Luiz Carlos da Silva Araujo on 18/09/21.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
