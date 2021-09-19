7ec8a4aa285db0ec73a05ab9114571ab879fa962
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2021-09-18 22:23:59 -0300

    :gift: Create the List of Landmarks

---
+++ swiftUI-015-Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,23 @@
+//
+//  LandmarkList.swift
+//  swiftUI-015-Landmarks
+//
+//  Created by Luiz Carlos da Silva Araujo on 18/09/21.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
