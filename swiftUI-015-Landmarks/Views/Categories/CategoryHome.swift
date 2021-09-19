4df577076057275f58f4dc610285791a3db780d3
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2021-09-19 15:52:26 -0300

    :gift: Compose Animations for Complex Effects

---
+++ swiftUI-015-Landmarks/Views/Categories/CategoryHome.swift
@@ -0,0 +1,24 @@
+//
+//  CategoryHome.swift
+//  swiftUI-015-Landmarks
+//
+//  Created by Luiz Carlos da Silva Araujo on 19/09/21.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationView {
+            Text("Hello, World!")
+                .navigationTitle("Featured")
+
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
