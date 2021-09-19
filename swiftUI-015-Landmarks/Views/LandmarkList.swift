2fae5b6b45e6479bbbb3b62a43d316258e0655a5
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2021-09-18 23:43:39 -0300

    :gift: Generate Previews Dynamically

--- swiftUI-015-Landmarks/Views/LandmarkList.swift
+++ swiftUI-015-Landmarks/Views/LandmarkList.swift
@@ -22,6 +22,10 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkList()
+        ForEach(["iPhone 8", "iPhone 12 Pro", "iPhone 12 Pro Max"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
     }
 }
