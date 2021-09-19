4fd8abe036c34135c81f403e41e942d9ecf4b120
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2021-09-19 13:08:28 -0300

    :gift: Draw the Badge Symbol

---
+++ swiftUI-015-Landmarks/Views/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,24 @@
+//
+//  RotatedBadgeSymbol.swift
+//  swiftUI-015-Landmarks
+//
+//  Created by Luiz Carlos da Silva Araujo on 19/09/21.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
